8d2dbe7b644df70ca7057389af12ef5b3e77e89c
KenZz <teamrafale.chaine@gmail.com>
2026-03-30 17:49:53 +0200

    Alignement menu à droite et fix loupe

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -12,47 +12,51 @@ function Header({ onSearch }) {
     }
   };
 
-  return (
-    <header className="container-fluid bg-light border-bottom p-3">
-      <div className="row align-items-center">
-        
-        {/* LOGO : Prend 12 colonnes sur mobile (centré), et 3 sur PC */}
-        <div className="col-12 col-md-3 text-center text-md-start mb-3 mb-md-0">
-          <Link to="/">
-            <img 
-              src="/img/Logo.png" 
-              alt="Trouve ton artisan" 
-              style={{ maxWidth: '180px', height: 'auto' }} 
-            />
-          </Link>
-        </div>
+return (
+  <header className="container-fluid bg-light border-bottom py-2">
+    <div className="row align-items-center g-2">
+      
+      {/* LOGO : Prend 12 colonnes sur mobile, 3 sur PC */}
+      <div className="col-12 col-md-3 text-center text-md-start">
+        <Link to="/">
+          <img 
+            id="logo" 
+            src="/img/Logo.png" 
+            alt="Trouve ton artisan" 
+            style={{ maxWidth: '180px', height: 'auto' }} 
+          />
+        </Link>
+      </div>
 
-        {/* RECHERCHE : Prend 12 colonnes sur mobile, et 4 sur PC */}
-        <div className="col-12 col-md-5 mb-3 mb-md-0">
-          <div className="input-group">
-            <input 
-              type="text" 
-              className="form-control" 
-              placeholder="Rechercher par nom..." 
-              onChange={handleInputChange} 
-            />
-            <span className="input-group-text bg-white">
-              <img src='/img/loupe.png' alt='Loupe' style={{ width: '18px' }} />
-            </span>
-          </div>
+      {/* BARRE DE RECHERCHE + LOUPE : Milieu */}
+      <div className="col-12 col-md-4">
+        <div className="input-group">
+          <input 
+            type="text" 
+            className="form-control shadow-none border-dark" 
+            placeholder="Rechercher par nom..." 
+            onChange={handleInputChange} 
+            style={{ borderRadius: '10px 0 0 10px' }}
+          />
+          <span className="input-group-text bg-white border-dark border-start-0" style={{ borderRadius: '0 10px 10px 0' }}>
+            <img src='/img/loupe.png' alt='Loupe' style={{ width: '18px' }} />
+          </span>
         </div>
+      </div>
 
-        {/* NAVIGATION : Prend 12 colonnes sur mobile, et 4 sur PC */}
-        <nav className="col-12 col-md-4 d-flex justify-content-center justify-content-md-end gap-2 flex-wrap header-nav">
+      {/* NAVIGATION : ms-auto pousse tout le bloc à droite sur PC */}
+      <nav className="col-12 col-md-5 ms-auto">
+        <div className="d-flex justify-content-center justify-content-md-end gap-3 flex-wrap">
           <Link className="nav-link-custom" to="/liste-artisans?categorie=Bâtiment">Bâtiment</Link>
           <Link className="nav-link-custom" to="/liste-artisans?categorie=Services">Services</Link>
           <Link className="nav-link-custom" to="/liste-artisans?categorie=Fabrication">Fabrication</Link>
           <Link className="nav-link-custom" to="/liste-artisans?categorie=Alimentation">Alimentation</Link>
-        </nav>
+        </div>
+      </nav>
 
-      </div>
-    </header>
-  );
+    </div>
+  </header>
+);
 }
 
 export default Header;
